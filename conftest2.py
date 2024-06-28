a8a0fe490a75f93473a3fd932565d40074f8373b
QA-Engeneer <F.M.Templar@ya.ru>
2024-06-28 17:10:39 +0500

    added fixture for catalog_and_subcatalog_page

--- conftest2.py
+++ conftest2.py
@@ -2,6 +2,7 @@ import pytest
 from conftest import chrome_browser as driver
 from interhsop5skillbox.pages.my_acc_page import MyAccountPage
 from interhsop5skillbox.pages.main_page import MainPage
+from interhsop5skillbox.pages.catalog_and_subcatalog_page import CatalogAndSubCatalogPage
 
 
 # Preparation work function for my_acc_page
@@ -20,3 +21,12 @@ def get_webdriver_instance_and_open_main_page(driver):
     main_page.open_page()
 
     return main_page
+
+
+# Preparation work function for catalog_and_subcatalog_page
+@pytest.fixture(scope="function")
+def get_webdriver_instance_and_open_catalog_and_subcatalog_page(driver):
+    catalog_and_sub_catalog_page = CatalogAndSubCatalogPage(driver)
+    catalog_and_sub_catalog_page.open_page()
+
+    return catalog_and_sub_catalog_page
