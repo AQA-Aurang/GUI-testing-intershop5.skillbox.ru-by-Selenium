0b0f9ed0e6994a1bcd08ed384d5681e30600f9fd
QA-Engeneer <F.M.Templar@ya.ru>
2024-06-28 08:58:22 +0500

    refactoring tests related to MyAccountPage and create new group tests for MainPage

---
+++ interhsop5skillbox/tests/test_main_page.py
@@ -0,0 +1,76 @@
+import time
+
+from selenium.webdriver.common.by import By
+from conftest import chrome_browser as driver
+import interhsop5skillbox.data.test_data as test_data
+import interhsop5skillbox.data.locators as locator
+from conftest2 import get_webdriver_instance_and_open_main_page as preparation_work
+from interhsop5skillbox.pages.base_page import get_element_in_another_element
+
+
+def test_go_to_sub_catalog_from_main_page(preparation_work):
+    main_page = preparation_work
+
+    main_page.go_to_sub_catalog_from_main_page(locator.first_sub_catalog, test_data.first_sub_catalog_title)
+
+    main_page.driver.back()
+    main_page.go_to_sub_catalog_from_main_page(locator.second_sub_catalog, test_data.second_sub_catalog_title)
+
+    main_page.driver.back()
+    main_page.go_to_sub_catalog_from_main_page(locator.third_sub_catalog, "", test_data.another_sub_catalog_title)
+
+
+def test_go_to_product_card_from_sale_block(preparation_work):
+    main_page = preparation_work
+
+    main_page.move_down_in_altitude_by(0.95)
+    product = main_page.get_element(By.XPATH, locator.product_in_sale_part)
+    product_title = get_element_in_another_element(product, By.TAG_NAME, locator.tag_a).get_attribute("title")
+    main_page.find_and_click_on_element(By.XPATH, f"{locator.product_in_sale_part}/div/a[@title='{product_title}']")
+    main_page.header_should_have_text(By.XPATH, locator.sub_catalog_in_product_page,
+                                      test_data.sub_catalog_in_product_page,
+                                      test_data.assertion_error_in_finding_product_in_product_page)
+
+
+def test_go_to_product_card_from_new_arrivals_block(preparation_work):
+    main_page = preparation_work
+
+    main_page.move_down_in_altitude_by(2.7)
+    product = main_page.get_element(By.XPATH, locator.product_in_arrival_part)
+    product_title = get_element_in_another_element(product, By.TAG_NAME, locator.tag_a).get_attribute("title")
+    main_page.find_and_click_on_element(By.XPATH, f"{locator.product_in_arrival_part}/div/a[@title='{product_title}']")
+    main_page.header_should_have_text(By.XPATH, locator.sub_catalog_in_product_page,
+                                      test_data.sub_catalog_in_product_page,
+                                      test_data.assertion_error_in_finding_product_in_product_page)
+
+
+def test_go_to_product_card_from_viewed_products(preparation_work):
+    main_page = preparation_work
+
+    main_page.move_down_in_altitude_by(0.95)
+    product = main_page.get_element(By.XPATH, locator.product_in_sale_part)
+    product_title = get_element_in_another_element(product, By.TAG_NAME, locator.tag_a).get_attribute("title")
+    main_page.find_and_click_on_element(By.XPATH, f"{locator.product_in_sale_part}/div/a[@title='{product_title}']")
+
+    actual_result = main_page.get_element(By.XPATH, locator.sub_catalog_in_product_page)
+    if actual_result.text == test_data.sub_catalog_in_product_page:
+        main_page.find_and_click_on_element(By.LINK_TEXT, locator.link_in_navbar_to_main_page)
+        main_page.move_down_in_altitude_by(5.5)
+
+        try:
+            viewed_products_block = main_page.get_element(By.ID, locator.viewed_products_block)
+            header_viewed_products = get_element_in_another_element(viewed_products_block, By.TAG_NAME, "h2")
+            viewed_product = get_element_in_another_element(viewed_products_block, By.TAG_NAME, "li")
+
+            if header_viewed_products.text == test_data.viewed_products.upper():
+                viewed_product_title = get_element_in_another_element(viewed_product, By.TAG_NAME, "span").text
+                viewed_product_title = viewed_product_title.replace("'", "’")
+
+                if viewed_product_title == product_title:
+                    element = get_element_in_another_element(viewed_product, By.TAG_NAME, "a")
+                    main_page.click_element(element)
+                    main_page.header_should_have_text(By.XPATH, locator.sub_catalog_in_product_page,
+                                            test_data.sub_catalog_in_product_page,
+                                            test_data.assertion_error_in_test_go_to_product_card_from_viewed_products)
+        except Exception as e:
+            print("Something went wrong -", e.args)
--- interhsop5skillbox/tests/test_my_acc.py
+++ interhsop5skillbox/tests/test_my_acc.py
@@ -12,7 +12,7 @@ def test_go_to_my_account_from_link_navbar(preparation_work):
     acc_page = preparation_work
     account = acc_page.get_element(By.LINK_TEXT, locator.my_account_link_in_my_account)
     acc_page.verify_account_title(account, test_data.title_in_account_page)
-    acc_page.header_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.main_header_in_account_page,
+    acc_page.element_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.main_header_in_account_page,
                                      test_data.assertion_error_in_test_go_to_my_account_from_link_navbar)
 
 
@@ -20,7 +20,7 @@ def test_go_to_my_account_from_login_link(preparation_work):
     acc_page = preparation_work
     account = acc_page.get_element(By.LINK_TEXT, locator.login_link_in_my_account)
     acc_page.verify_account_title(account, test_data.title_in_account_page)
-    acc_page.header_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.main_header_in_account_page,
+    acc_page.element_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.main_header_in_account_page,
                                      test_data.assertion_error_in_test_go_to_my_account_from_login_link)
 
 
@@ -29,7 +29,7 @@ def test_go_to_my_account_from_footer_link(preparation_work):
     acc_page.go_to_bottom()
     account = acc_page.get_element(By.LINK_TEXT, locator.my_account_link_in_my_account)
     acc_page.verify_account_title(account, test_data.title_in_account_page)
-    acc_page.header_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.main_header_in_account_page,
+    acc_page.element_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.main_header_in_account_page,
                                      test_data.assertion_error_in_test_go_to_my_account_from_footer_link)
 
 
@@ -39,7 +39,7 @@ def test_go_to_orders_from_info_block_after_auth(preparation_work, login, logout
     acc_page.driver = login
     link_to_orders = acc_page.get_element(By.LINK_TEXT, locator.my_orders_link_in_my_account)
     acc_page.driver = acc_page.verify_account_title(link_to_orders, test_data.title_in_account_page)
-    acc_page.header_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.orders_text_for_assertion,
+    acc_page.element_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.orders_text_for_assertion,
                                      test_data.assertion_error_in_test_go_to_orders_from_info_block_after_auth)
     logout
 
@@ -49,7 +49,7 @@ def test_go_to_orders_after_auth(preparation_work, login, logout):
     acc_page.driver = login
     link_to_orders = acc_page.get_element(By.LINK_TEXT, locator.link_to_orders_block_in_my_account)
     acc_page.click_element(link_to_orders)
-    acc_page.header_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.orders_text_for_assertion,
+    acc_page.element_should_have_text(By.CLASS_NAME, locator.title_in_my_account, test_data.orders_text_for_assertion,
                                      test_data.assertion_error_in_test_go_to_orders_after_auth)
     logout
 
@@ -103,14 +103,14 @@ def test_modify_password_fields(preparation_work, login, logout):
     acc_page.driver = login
     acc_page.driver = acc_page.navigation_to_personal_details()
     acc_page.driver = acc_page.change_password_fields(acc_page.default_password, test_data.new_password, test_data.new_password)
-    acc_page.header_should_have_text(By.CLASS_NAME, locator.alert_notification_in_personal_block_in_my_account,
+    acc_page.element_should_have_text(By.CLASS_NAME, locator.alert_notification_in_personal_block_in_my_account,
                                      test_data.success_change_personal_data_for_assertion,
                                      test_data.assertion_error_in_test_modify_password_fields)
 
     # we have to check, need logout and try login with old password
     acc_page.driver = acc_page.logout_and_login()
 
-    acc_page.header_should_have_text(By.XPATH, locator.alert_about_non_success_change_password,
+    acc_page.element_should_have_text(By.XPATH, locator.alert_about_non_success_change_password,
                                      test_data.failure_text_in_change_password_for_assertion,
                                      test_data.assertion_error_in_test_modify_password_fields)
     acc_page.revert_password(test_data.new_password)
@@ -123,7 +123,7 @@ def test_modify_password_without_current_pass(preparation_work, login, logout):
     acc_page.driver = login
     acc_page.driver = acc_page.navigation_to_personal_details()
     acc_page.driver = acc_page.change_password_fields("", test_data.new_password, test_data.new_password)
-    acc_page.header_should_have_text(By.XPATH, locator.alert_about_non_success_change_password, test_data.alert_text_for_assertion,
+    acc_page.element_should_have_text(By.XPATH, locator.alert_about_non_success_change_password, test_data.alert_text_for_assertion,
                                      test_data.assertion_error_in_test_modify_password_without_current_pass)
     logout
 
@@ -133,7 +133,7 @@ def test_mismatched_new_pass_with_repeat_new_pass_fields(preparation_work, login
     acc_page.driver = login
     acc_page.driver = acc_page.navigation_to_personal_details()
     acc_page.driver = acc_page.change_password_fields(acc_page.default_password, test_data.new_password, test_data.repeat_new_password)
-    acc_page.header_should_have_text(By.XPATH, locator.alert_about_non_success_change_password,
+    acc_page.element_should_have_text(By.XPATH, locator.alert_about_non_success_change_password,
                                      test_data.password_not_match_for_assertion,
                                      test_data.assertion_error_in_test_mismatched_new_pass_with_repeat_new_pass_fields)
     logout
@@ -144,5 +144,5 @@ def test_logout_by_link_in_account(preparation_work, login):
     acc_page.driver = login
     acc_page.driver = acc_page.navigation_to_personal_details()
     acc_page.find_and_click_on_element(By.XPATH, locator.logout_link_in_personal_block_in_my_account)
-    acc_page.header_should_have_text(By.LINK_TEXT, locator.login_link_in_my_account, test_data.isLogin,
+    acc_page.element_should_have_text(By.LINK_TEXT, locator.login_link_in_my_account, test_data.isLogin,
                                      test_data.assertion_error_test_logout_by_link_in_account)
