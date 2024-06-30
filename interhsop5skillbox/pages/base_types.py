2e1cdefb00cc923143c5a6bda1187d9f7b9e1824
QA-Engeneer <F.M.Templar@ya.ru>
2024-06-30 18:38:23 +0500

    added 2 different classes to work with two different fixtures that create a browser with excellent customizations

---
+++ interhsop5skillbox/pages/base_types.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from conftest import chrome_browser as chrome
+from conftest import chrome_browser_long_timeout as chrome_lt
+
+
+class BaseType(ABC):
+    @property
+    def get_driver(self):
+        pass
+
+
+class PageWithChromeBrowser(BaseType):
+    def __init__(self, chrome):
+        self._driver = chrome
+
+    @property
+    def get_driver(self):
+        return self._driver
+
+
+class PageWithChromeBrowserLongTimeout(BaseType):
+    def __init__(self, chrome_lt):
+        self._driver = chrome_lt
+
+    @property
+    def get_driver(self):
+        return self._driver
