3ca577fd5e154d0dc8ea278e7340d25914bedc10
QA-Engeneer <F.M.Templar@ya.ru>
2024-06-30 18:42:56 +0500

    added additional data

--- interhsop5skillbox/data/locators.py
+++ interhsop5skillbox/data/locators.py
@@ -4,6 +4,24 @@ my_account_link_in_my_account = "Мой аккаунт"
 
 login_link_in_my_account = "Войти"
 
+logout_link = "Выйти"
+
+login_button = "login"
+
+username_id = "username"
+
+password_id = "password"
+
+account_data = "Данные аккаунта"
+
+current_password = "password_current"
+
+new_password = "password_1"
+
+repeat_new_password = "password_2"
+
+save_button_for_changed_data = "save_account_details"
+
 my_orders_link_in_my_account = "свои заказы"
 
 link_to_orders_block_in_my_account = "Заказы"
@@ -20,7 +38,7 @@ email_field_in_personal_block_in_my_account = "account_email"
 
 alert_notification_in_personal_block_in_my_account = "woocommerce-message"
 
-alert_about_non_success_change_password = "//ul[@role='alert']//li[1]"
+not_success_alert = "//ul[@role='alert']//li[1]"
 
 logout_link_in_personal_block_in_my_account = "(//nav[@class='woocommerce-MyAccount-navigation']//li)[4]/a"
 
@@ -87,3 +105,93 @@ search_field = "(//input[@name='s'])[1]"
 product_links = "(//a[@class='collection_title']//h3)"
 
 search_button = "searchsubmit"
+
+order_link = "ОФОРМИТЬ ЗАКАЗ"
+
+link_for_coupon_block_in_order_page = "Нажмите для ввода купона"
+
+overlay_block = "(//div[@class='blockUI blockOverlay'])[1]"
+
+overlay_block2 = "(//div[@class='blockUI blockOverlay'])[2]"
+
+link_to_remove_coupon = "[Удалить]"
+
+notification_about_deleted_coupon = "//div[text()[normalize-space()='Купон удален.']]"
+
+name_field_in_order_page = "billing_first_name"
+
+last_name_field_in_order_page = "billing_last_name"
+
+city_field_in_order_page = "billing_city"
+
+apply_button_in_order_page = "place_order"
+
+notification_element_about_empty_field = "//li[@data-id='billing_first_name']"
+
+few_errors_by_empty_mandatory_fields = "//ul[@class='woocommerce-error']/li"
+
+quantity = "quantity"
+
+add_to_cart_button = "add-to-cart"
+
+notification_add_to_cart = "//a[@class='button wc-forward']"
+
+quantity_products_on_product_card_page = "//p[@class='stock in-stock']"
+
+notification_element = "//div[@role='alert']"
+
+notification_about_add_product_in_cart = "//div[@role='alert']/text()"
+
+magnifying_glass = "woocommerce-product-gallery__trigger"
+
+zoom_window = "pswp__img"
+
+order_in_order_table = "(//td[@data-title='Заказ']//a)[1]"
+
+ordered_product = "//td[@class='woocommerce-table__product-name product-name']"
+
+review_tab = "//a[@href='#tab-reviews']"
+
+stars = ".stars>span>a"
+
+comment_field_for_review = "//textarea[@id='comment']"
+
+review_button = "submit"
+
+categories = "//li[contains(@class,'cat-item')]/a"
+
+title_of_page_selected_categories = "//h1[@class='entry-title ak-container']"
+
+detail_link = "//a[@title='Подробнее']"
+
+block_of_products_in_left_side = "//ul[@class='product_list_widget']//li"
+
+title_of_searching_product = "//h1[@class='product_title entry-title']"
+
+img_element = "//img[@loading='lazy']"
+
+count_of_product_on_cart_page = "//input[@inputmode='numeric']"
+
+field_for_coupon_input = "coupon_code"
+
+apply_coupon_on_cart_page = "apply_coupon"
+
+recovery_product = "Вернуть?"
+
+popup_block_element_of_review_for_product = "//div[@class='wp-die-message']//p[1]"
+
+coupon_success_applied = "//div[text()[normalize-space()='Купон успешно добавлен.']]"
+
+element_in_dropdown = "//li[@id='menu-item-119']/a[1]"
+
+selected_element_in_sorting_block = "//option[@selected='selected']"
+
+order_detail = "Подробнее"
+
+product_in_sale_block_on_main_page = "(//li[@data-slick-index='0'])[1]/div/a"
+
+product_card_footers = "//ul[@class='products columns-4']//li/div[2]"
+
+button_in_product_footer = "//ul[@class='products columns-4']//li/div[2]/div/a"
+
+discount_row = "//th[contains(text(), 'Скидка')]"
--- interhsop5skillbox/data/test_data.py
+++ interhsop5skillbox/data/test_data.py
@@ -10,6 +10,10 @@ assertion_error_in_test_go_to_my_account_from_footer_link = "Cannot go to my acc
 
 orders_text_for_assertion = "Заказы"
 
+order = "Order"
+
+assertion_error_cannot_get_detail_for_order = "Cannot get more data about order"
+
 assertion_error_in_test_go_to_orders_from_info_block_after_auth = "Cannot go to orders from info block"
 
 assertion_error_in_test_go_to_orders_after_auth = "Cannot go to orders by orders"
@@ -76,8 +80,62 @@ notification_absent_txt_for_assertion = "По вашему запросу тов
 
 assertion_not_equals_page_numbers = "Pagination page not equals"
 
-assertion_error_in_test_go_to_product_from_block_under_the_filter = "Titles of products is not equals"
+assertion_error_not_equals_text_with_title_page = "Titles of products is not equals"
 
 assertion_error_in_test_go_to_product_from_search_field = "Search product not equals with product in search output page"
 
 search_product = "watch"
+
+promo_code = "GIVEMEHALYAVA"
+
+certificate = "SERT500"
+
+not_exist_coupon = "Pedro-pedro-pedro_Pe"
+
+coupon_success_applied = "Купон успешно добавлен."
+
+assertion_error_cannot_applied_coupon = "Coupon was not applied"
+
+notification_about_already_applied_coupon = "Coupon code already applied!"
+
+coupon_removed = "Купон удален."
+
+assertion_error_cannot_remove_coupon = "Coupon could not be deleted"
+
+name_field_notification_for_assertion = "Имя для выставления счета обязательное поле."
+
+last_name_field_notification_for_assertion = "Фамилия для выставления счета обязательное поле."
+
+city_field_notification_for_assertion = "Город / Населенный пункт для выставления счета обязательное поле."
+
+assertion_error_test_place_order_with_empty_mandatory_field = "Cannot order the product with empty name filed"
+
+assertion_error_test_place_order_with_some_empty_mandatory_fields = "Cannot order the product with some empty fields"
+
+assertion_error_test_add_product_to_cart = "Can't added product in cart"
+
+assertion_error_test_adding_more_items_than_are_in_stock = "Got to add more products to the cart than in stock"
+
+assertion_error_test_zoom_element_in_product_card1 = "Zoom element is disable"
+
+assertion_error_test_zoom_element_in_product_card2 = "Product doesn't have picture to show"
+
+assertion_error_failed_to_sent_review = "Review not sent"
+
+assertion_error_in_sideblock_on_product_page = "Title selected category not equal title of page"
+
+product_not_visible = "Element not visible"
+
+product_not_found_in_cart = "To cart not found"
+
+assertion_error_in_test_remove_product_added_in_cart = "Cannot remove product from cart"
+
+assertion_error_test_recovery_product_after_removing = "Deleted product does not match the recovered product"
+
+incorrect_coupon = "Неверный купон."
+
+assertion_error_with_incorrect_coupon = "Coupon 'Pedro-pedro-pedro_Pe' doesn't exist"
+
+no_orders_yet = "Заказов еще нет."
+
+orders_been_placed = "Orders have been placed"
