d83e2b34c9f121f5755b4178db7002b4f6f2777b
AQA-Faridun <f.m.templar@ya.ru>
2024-09-11 23:11:19 +0500

    Type annotation added in variable, function arguments and return values related main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
-import time
+from typing import Tuple
 
+import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.remote.webelement import WebElement
 from pages.base_page import BasePage
-from .product_card_page import ProductPage
+from pages.product_card_page import ProductPage
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
@@ -24,46 +25,68 @@ class MainPage(BasePage):
             raise Exception(f"This is not main page, current page is: {self.driver.current_url}")
 
     def check_and_go_back_in_main_page(self):
-        if self.get_title() != "Skillbox — Интернет магазин":
-            self.driver.get("https://intershop5.skillbox.ru")
+        with allure.step('Check main page'):
+            if self.get_title() != "Skillbox — Интернет магазин":
+                # self.driver.get("https://intershop5.skillbox.ru")
+                self.load()
 
-    def get_catalog_and_title(self, item):
-        catalogs = self.wait_for_elements(self.CATALOGS)
-        catalog = catalogs[item]
-        catalog_title = self.get_element_from_another_element(catalog, By.TAG_NAME, "h4").text.capitalize()
+    def get_catalog_and_title(self, item: int) -> tuple[WebElement, str]:
+        with allure.step('Get catalogs'):
+            catalogs: list[WebElement] = self.wait_for_elements(self.CATALOGS)
+
+        with allure.step('Choose one of catalog'):
+            catalog: WebElement = catalogs[item]
+
+        with allure.step('Get title of choosen catalog'):
+            catalog_title: str = self.get_element_from_another_element(catalog, By.TAG_NAME, "h4").text.capitalize()
 
         return catalog, catalog_title
 
-    def go_to_product_from_sales_section(self, item):
-        products = self.wait_for_elements(self.PRODUCTS_FROM_SALES_SECTION)
-        product = products[item]
-        self.scroll_to_element(product)
-        header = self.get_element_from_another_element(product, By.TAG_NAME, "a").get_attribute("title")
-        product.click()
+    def go_to_product_from_sales_section(self, item: int) -> tuple[ProductPage, str]:
+        with allure.step('Get products from sales section'):
+            products: list[WebElement] = self.wait_for_elements(self.PRODUCTS_FROM_SALES_SECTION)
+
+        with allure.step('Get one of the product and scroll to it'):
+            product: WebElement = products[item]
+            self.scroll_to_element(product)
+
+        with allure.step('Get product title'):
+            header: str = self.get_element_from_another_element(product, By.TAG_NAME, "a").get_attribute("title")
+
+        with allure.step('Click on product'):
+            product.click()
 
         return ProductPage(self.driver, header), header
 
-    def go_to_product_from_new_arrivals_section(self, item):
-        products = self.wait_for_elements(self.PRODUCTS_FROM_NEW_ARRIVALS_SECTION)
-        product = products[item]
-        self.scroll_to_element(product)
-        header = self.get_element_from_another_element(product, By.TAG_NAME, "a").get_attribute("title")
-        product.click()
+    def go_to_product_from_new_arrivals_section(self, item: int) -> tuple[ProductPage, str]:
+        with allure.step('Get products from arrivals section'):
+            products: list[WebElement] = self.wait_for_elements(self.PRODUCTS_FROM_NEW_ARRIVALS_SECTION)
+
+        with allure.step('Get one of the product and scroll to it'):
+            product: WebElement = products[item]
+            self.scroll_to_element(product)
+
+        with allure.step('Get product title'):
+            header: str = self.get_element_from_another_element(product, By.TAG_NAME, "a").get_attribute("title")
+
+        with allure.step('Click on product'):
+            product.click()
 
         return ProductPage(self.driver, header), header
 
-    def get_product_and_title_from_poster_section(self):
-        poster = self.wait_for_element(self.PRODUCT_FROM_POSTER_SECTION)
-        self.scroll_to_element(poster)
+    def get_product_and_title_from_poster_section(self) -> tuple[ProductPage, str]:
+        with allure.step('Get poster and scroll to it'):
+            poster: WebElement = self.wait_for_element(self.PRODUCT_FROM_POSTER_SECTION)
+            self.scroll_to_element(poster)
 
-        time.sleep(2)
-        # url = self.get_element_from_another_element(poster, By.TAG_NAME, "a", 15).get_attribute("href")
-        product_title = self.get_element_from_another_element(poster, By.CLASS_NAME, "promo-desc-title").text
-        product_button = self.get_element_from_another_element(poster, By.XPATH, "(//span[@class='btn promo-link-btn'])[4]")
+        with allure.step('Get product title and button'):
+            product_title: str = self.get_element_from_another_element(poster, By.CLASS_NAME, "promo-desc-title").text
+            product_button: WebElement = self.wait_for_element((By.XPATH, "(//span[@class='btn promo-link-btn'])[4]"))
 
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable(product_button))
-        product_button.click()
+        with allure.step('Wait for the button to appear and click on it'):
+            wait = WebDriverWait(self.driver, 10)
+            wait.until(EC.element_to_be_clickable(product_button))
+            product_button.click()
 
         # def get_product_name_by(url):
         #     text = url.split('=')[-1]
@@ -80,37 +103,40 @@ class MainPage(BasePage):
 
         return ProductPage(self.driver, product_title), product_title
 
-    def go_to_viewed_product(self, item):
+    def go_to_viewed_product(self, item: int) -> tuple[ProductPage, str]:
         header = ""
         try:
-            products = self.wait_for_elements(self.PRODUCTS_FROM_VIEWED_PRODUCTS_SECTION)
-            # print("кол-во элементов в products -", len(products))
-            product = products[item]
-            self.scroll_to_element(product)
-            header = product.find_element(By.TAG_NAME, "span").text
+            with allure.step('Get products from viewed section'):
+                products: list[WebElement] = self.wait_for_elements(self.PRODUCTS_FROM_VIEWED_PRODUCTS_SECTION)
 
-            if '"Холодец-4"' in header:
-                header = self.replace_quotes(header)
-                # print("new header -", header)
+            with allure.step('Get one of the product and scroll to it'):
+                product: WebElement = products[item]
+                self.scroll_to_element(product)
 
-            product_link = product.find_element(By.TAG_NAME, "a")
-            self.click_by(product_link)
+            with allure.step('Get product title'):
+                header: str = product.find_element(By.TAG_NAME, "span").text
+
+            # with allure.step('Check product title for text - "Холодец-4"'):
+                # if '"Холодец-4"' in header:
+                #     header: str = self.replace_quotes(header)
+
+            with allure.step('Get product link and go to it'):
+                product_link: WebElement = product.find_element(By.TAG_NAME, "a")
+                self.click_by(product_link)
 
         except TimeoutException:
             print("Cannot find viewed product block")
 
         return ProductPage(self.driver, header), header
 
-    def replace_quotes(self, text):
-        """Replaces double quotes with herringbones in the given text."""
-
-        new_txt = text.replace('"', '«')
-
-        # Ищем индекс последней левой кавычки
-        last_index = new_txt.rfind('«')
-
-        # Если кавычка найдена, заменяем ее
-        if last_index != -1:
-            return new_txt[:last_index] + '»' + new_txt[last_index + 1:]
-
-        return new_txt
+    # def replace_quotes(self, text: str) -> str:
+    #     """Replaces double quotes with herringbones in the given text."""
+    #
+    #     with allure.step('Replace one type of quotes `""` to another type `«»`'):
+    #         new_txt = text.replace('"', '«')
+    #         last_index = new_txt.rfind('«')
+    #
+    #         if last_index != -1:
+    #             return new_txt[:last_index] + '»' + new_txt[last_index + 1:]
+    #
+    #         return new_txt
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,53 +1,61 @@
-import time
-
 import pytest
-
+import allure
 from pages.catalog_and_category_page import CatalogAndCategoryPage
+from main_page import MainPage
 
 
+@allure.feature('Testing main page')
 @pytest.mark.usefixtures('chrome_browser')
 class Tests:
 
+    @allure.story('testing link for go to sub catalog from main page')
     @pytest.mark.parametrize('serial_number', [0, 1, 2])
-    def test_go_to_sub_catalog_from_main_page(self, prepare_main_page, serial_number):
-        prepare_main_page.check_and_go_back_in_main_page()
-
-        catalog, title = prepare_main_page.get_catalog_and_title(serial_number)
+    def test_go_to_sub_catalog_from_main_page(self, main_page: MainPage, serial_number: int):
+        catalog, title = main_page.get_catalog_and_title(serial_number)
         catalog.click()
 
         if title == "Фотоаппараты":
             title = "Фото/видео"
 
-        sub_catalog_of_pad = CatalogAndCategoryPage(prepare_main_page.driver, title)
+        sub_catalog: CatalogAndCategoryPage = CatalogAndCategoryPage(main_page.driver, title)
 
-        assert sub_catalog_of_pad.get_title() == title, "Couldn't get a catalog"
+        with allure.step('Check for match titles between sub catalog in main page and sub catalog page'):
+            assert sub_catalog.get_title() == title, "Couldn't get a catalog"
 
-    @pytest.mark.parametrize('visible_product', [4, 5])
-    def test_go_to_product_card_from_sale_block(self, prepare_main_page, visible_product):
-        prepare_main_page.check_and_go_back_in_main_page()
+    @allure.story('testing link for go to product card from sales block')
+    @pytest.mark.parametrize('visible_product', [5, 6])
+    def test_go_to_product_card_from_sale_block(self, main_page: MainPage, visible_product: int):
+        product, title = main_page.go_to_product_from_sales_section(visible_product)
 
-        product, title = prepare_main_page.go_to_product_from_sales_section(visible_product)
-        assert product.get_title() == title.capitalize(), "Couldn't get a product from sales block"
+        with allure.step('Check for match titles between product in main page and product page'):
+            assert product.get_title() == title.capitalize(), "Couldn't get a product from sales block"
 
-    @pytest.mark.xfail(reason="Incorrect behavior of the program, it was expected to match the names of products both on "
-                              "the product page and in the product card, but we get no match")
-    def test_go_to_product_from_poster(self, prepare_main_page):
-        prepare_main_page.check_and_go_back_in_main_page()
+    @allure.story('testing button in poster block for go to product page')
+    @pytest.mark.xfail(
+        reason="Incorrect behavior of the program, it was expected to match the names of products both on "
+               "the product page and in the product card, but we get no match")
+    def test_go_to_product_from_poster(self, main_page: MainPage):
+        product, title = main_page.get_product_and_title_from_poster_section()
 
-        product, title = prepare_main_page.get_product_and_title_from_poster_section()
+        with allure.step('Check for match titles between product in main page and product page'):
+            assert product.get_title() == title.capitalize(), "Couldn't get a product from poster section"
 
-        assert product.get_title() == title.capitalize(), "Couldn't get a product from poster section"
+    @allure.story('testing links for product from arrivals block')
+    @pytest.mark.parametrize('visible_product', [5, 6])
+    def test_go_to_product_card_from_new_arrivals_block(self, main_page: MainPage, visible_product: int):
+        product, title = main_page.go_to_product_from_new_arrivals_section(visible_product)
 
-    @pytest.mark.parametrize('visible_product', [4, 6])
-    def test_go_to_product_card_from_new_arrivals_block(self, prepare_main_page, visible_product):
-        prepare_main_page.check_and_go_back_in_main_page()
+        with allure.step('Check for match titles between product in main page and product page'):
+            assert product.get_title() == title.capitalize(), "Couldn't get a product from arrivals block"
 
-        product, title = prepare_main_page.go_to_product_from_new_arrivals_section(visible_product)
-        assert product.get_title() == title.capitalize(), "Couldn't get a product from arrivals block"
+    @allure.story('testing links for product from viewed products')
+    def test_go_to_product_card_from_viewed_products(self, main_page: MainPage):
+        main_page.go_to_product_from_sales_section(7)
+        main_page.load()
+        main_page.go_to_product_from_sales_section(8)
+        main_page.load()
 
-    def test_go_to_product_card_from_viewed_products(self, prepare_main_page):
-        prepare_main_page.check_and_go_back_in_main_page()
+        product, title = main_page.go_to_viewed_product(1)
 
-        product, title = prepare_main_page.go_to_viewed_product(1)
-
-        assert product.get_title() == title.capitalize(), "Couldn't get a product from viewed section"
+        with allure.step('Check for match titles between product in main page and product page'):
+            assert product.get_title() == title.capitalize(), "Couldn't get a product from viewed section"
