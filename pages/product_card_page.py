4d9c760bc34a3b80f3e328c2b103f0dad584533b
AQA-Faridun <f.m.templar@ya.ru>
2024-09-13 22:38:27 +0500

    added annotation types, modify autotest related with feedback to product

--- pages/product_card_page.py
+++ pages/product_card_page.py
@@ -1,58 +1,56 @@
-import random
-import time
-
+from typing import Union
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
-
-# from conftest2 import chrome_browser as driver
-# from conftest2 import get_username_password
-# from conftest2 import get_webdriver_instance_and_open_catalog_and_sub_catalog_page as preparation_work_catalog_page
-from .base_page import BasePage
-from pages.catalog_and_category_page import CatalogAndCategoryPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, InvalidSelectorException
 
+from pages.order_detail_page import OrderDetailPage
+from pages.base_page import BasePage
+from pages.order_page import OrderPage
+from pages.catalog_and_category_page import CatalogAndCategoryPage
+from pages.my_account_page import MyAccountPage
 
-def get_ordering_product(my_account_page, username, password, order_num=0):
-    my_account_page.authorisation(username, password)
 
-    order_page = my_account_page.go_to_order_block()
-    orders = order_page.get_orders()
+def get_ordering_product(my_account_page: MyAccountPage, username: str, password: str, order_num: int = 0) -> str:
+
+    order_page: OrderPage = my_account_page.go_to_order_block()
+    orders: list[WebElement] = order_page.get_orders()
     title, link = order_page.get_title_and_link(orders[order_num], order_num)
-    order_detail_page = order_page.click_by_this(link)
+    order_detail_page: OrderDetailPage = order_page.click_by_this(link)
     product = order_detail_page.get_product()
-    product_title = product.text
+    product_title: str = product.text
 
     order_detail_page.click_by(product)
     return product_title
 
 
-def get_any_product_from_catalog(preparation_work, item):
-    catalog_and_category_page = preparation_work
-    products = catalog_and_category_page.get_products()
-    product = products[item]
+def get_any_product_from_catalog(catalog_and_sub_catalog_page: CatalogAndCategoryPage, item: int) -> tuple[Union[webdriver.Chrome, webdriver.Firefox, webdriver.Edge], str]:
+    products: list[WebElement] = catalog_and_sub_catalog_page.get_products()
+    product: WebElement = products[item]
 
-    product_txt = product.text.split("\n")[0]
+    product_txt: str = product.text.split("\n")[0]
     if "₽" not in product_txt:
-        product_title = product.text.split("\n")[1] if product_txt == "Скидка!" else product_txt
+        product_title: str = product.text.split("\n")[1] if product_txt == "Скидка!" else product_txt
     else:
-        product_title = ""
+        product_title: str = ""
 
-    product_link = catalog_and_category_page.get_element_from_another_element(product, By.TAG_NAME, "a")
+    product_link: WebElement = catalog_and_sub_catalog_page.get_element_from_another_element(product, By.TAG_NAME, "a")
     product_link.click()
 
-    return catalog_and_category_page.driver, product_title
+    return catalog_and_sub_catalog_page.driver, product_title
 
 
-def get_any_product_from_catalog_light_version(catalog_and_category_page, item):
-    products = catalog_and_category_page.get_products()
-    product = products[item]
-    product_title = product.text.split("\n")[1]
+def get_any_product_from_catalog_light_version(catalog_and_sub_catalog_page: CatalogAndCategoryPage, item: int) -> tuple[Union[webdriver.Chrome, webdriver.Firefox, webdriver.Edge], str]:
+    products: CatalogAndCategoryPage = catalog_and_sub_catalog_page.get_products()
+    product: WebElement = products[item]
+    product_title: str = product.text.split("\n")[1]
 
-    product_link = catalog_and_category_page.get_element_from_another_element(product, By.TAG_NAME, "a")
+    product_link: WebElement = catalog_and_sub_catalog_page.get_element_from_another_element(product, By.TAG_NAME, "a")
     product_link.click()
 
-    return catalog_and_category_page.driver, product_title
+    return catalog_and_sub_catalog_page.driver, product_title
 
 
 class ProductPage(BasePage):
@@ -73,45 +71,45 @@ class ProductPage(BasePage):
     COME_BACK_LINK = (By.LINK_TEXT, "« Back")
     DUPLICATE_WARNING = (By.XPATH, "//div[@class='wp-die-message']//p[1]")
 
-    def __init__(self, driver, product_name):
+    def __init__(self, driver, product_name: str):
         super().__init__(driver)
         self.product_name = product_name
 
         if self.driver.title != f"{product_name} — Skillbox":
             raise Exception(f"This is not {product_name}, current product name is: {self.driver.title} on page is: {self.driver.current_url}")
 
-    def get_title(self):
+    def get_title(self) -> str:
         return self.wait_for_element(self.PRODUCT_TITLE).text.capitalize()
 
-    def is_quantity_field_available(self):
+    def is_quantity_field_available(self) -> bool:
         try:
             self.wait_for_element(self.PRODUCT_QUANTITY)
             return True
         except TimeoutException:
             return False
 
-    def change_count_buying_product(self, count):
+    def change_count_buying_product(self, count: int) -> None:
         if self.is_available_in_stock():
             self.type(self.PRODUCT_QUANTITY, count)
 
-    def is_magnifying_glass_available(self):
+    def is_magnifying_glass_available(self) -> bool:
         try:
             self.element_is_clickable(self.MAGNIFYING_GLASS)
             return True
         except TimeoutException:
             return False
 
-    def click_to_magnifying_glass(self):
+    def click_to_magnifying_glass(self) -> None:
         self.click(self.MAGNIFYING_GLASS)
 
-    def is_available_in_stock(self):
+    def is_available_in_stock(self) -> bool:
         try:
             self.wait_for_element(self.IN_STOCK_OR_NOT)
             return False
         except TimeoutException:
             return True
 
-    def how_many_product_in_stock(self):
+    def how_many_product_in_stock(self) -> int:
         if self.is_available_in_stock():
             try:
                 product_count_in_stock = self.get_text_of_element(self.PRODUCT_COUNT_IN_STOCK)
@@ -119,42 +117,42 @@ class ProductPage(BasePage):
             except TimeoutException:
                 return 0
 
-    def add_product_to_cart(self):
+    def add_product_to_cart(self) -> None:
         self.click(self.CART_BUTTON)
 
-    def switch_to_feedback_tab(self):
-        add_to_card_button = self.wait_for_element(self.CART_BUTTON)
+    def switch_to_feedback_tab(self) -> None:
+        add_to_card_button: WebElement = self.wait_for_element(self.CART_BUTTON)
         self.scroll_to_element(add_to_card_button) # scroll to button to add product in cart for see feedback tab and click one
         self.click(self.FEEDBACK_TAB)
 
-    def is_comment_field_available(self):
+    def is_comment_field_available(self) -> bool:
         try:
             self.wait_for_element(self.FEEDBACK_COMMENT)
             return True
         except TimeoutException:
             return False
 
-    def leave_feedback(self, star, comment):
-        marks = self.wait_for_elements(self.FEEDBACK_MARKS)
-        mark = marks[star-1]
+    def leave_feedback(self, star: int, comment: str) -> None:
+        marks: list[WebElement] = self.wait_for_elements(self.FEEDBACK_MARKS)
+        mark: int = marks[star-1]
         mark.click()
 
         self.type(self.FEEDBACK_COMMENT, comment)
         self.click(self.FEEDBACK_BUTTON)
 
-    def get_products_from_related_products(self):
+    def get_products_from_related_products(self) -> list[WebElement]:
         return self.wait_for_elements(self.PRODUCTS_FROM_RELATED_BLOCK)
 
-    def go_to_related_product(self, secret_word="Read more"):
-        products = self.get_products_from_related_products()
-        product_title = ""
+    def go_to_related_product(self, secret_word: str = "Read more") -> tuple[Union[webdriver.Chrome, webdriver.Firefox, webdriver.Edge], str]:
+        products: list[WebElement] = self.get_products_from_related_products()
+        product_title: str = ""
 
         for product in products:
             if secret_word in product.text:
                 try:
-                    product_link = self.get_element_from_another_element(product, By.CLASS_NAME, "button.product_type_simple")
-                    product_title = product.text.split("\n")[0] if "₽" in product.text.split("\n")[1] \
-                                                                else product.text.split("\n")[1]
+                    product_link: WebElement = self.get_element_from_another_element(product, By.CLASS_NAME, "button.product_type_simple")
+                    product_title: str = product.text.split("\n")[0] if "₽" in product.text.split("\n")[1] \
+                        else product.text.split("\n")[1]
                     self.scroll_to_element(product)
                     self.click_by(product_link)
                     break
@@ -170,32 +168,33 @@ class ProductPage(BasePage):
 
         return self.driver, product_title
 
-    def add_related_product_to_cart(self):
+    def add_related_product_to_cart(self) -> tuple[Union[webdriver.Chrome, webdriver.Firefox, webdriver.Edge], str]:
         return self.go_to_related_product("В корзину".upper())
 
-    def go_to_cart_after_add_related_product(self):
-        detail_link = self.wait_for_element((By.XPATH, "//a[@class='added_to_cart wc-forward']"))
+    def go_to_cart_after_add_related_product(self) -> None:
+        detail_link: WebElement = self.wait_for_element((By.XPATH, "//a[@class='added_to_cart wc-forward']"))
         self.click_by(detail_link)
 
-    def get_categories_from_goods_category_block(self):
+    def get_categories_from_goods_category_block(self) -> list[WebElement]:
         return self.wait_for_elements(self.CATEGORY_FROM_CATEGORIES_BLOCK)
 
-    def get_products_from_goods_block(self):
+    def get_products_from_goods_block(self) -> list[WebElement]:
         return self.wait_for_elements(self.PRODUCTS_FROM_GOODS_BLOCK)
 
-    def is_exist_feedback(self, comment, timeout=10):
+    def is_exist_feedback(self, comment: str, timeout: int = 10) -> bool:
         self.driver.implicitly_wait(0)
         try:
             wait = WebDriverWait(self.driver, timeout)
             wait.until(EC.presence_of_all_elements_located((By.XPATH, f"//p[text()='{comment}']")))
             return True
         except TimeoutException as te:
-            print("error -", te.msg)
+            # print("timeout exception -", te.msg)
+            self.logger.error(f"timeout exception: {te.msg}")
             return False
 
-    def go_back_when_detect_duplicate_feedback(self):
+    def go_back_in_detect_duplicate_feedback(self) -> None:
         if "Duplicate comment detected;" in self.wait_for_element(self.DUPLICATE_WARNING).text:
             self.click(self.COME_BACK_LINK)
 
-    def get_all_products_from_goods_block(self):
+    def get_all_products_from_goods_block(self) -> list[WebElement]:
         return self.wait_for_elements(self.PRODUCTS_FROM_GOODS_BLOCK)
--- tests/test_product_card.py
+++ tests/test_product_card.py
@@ -1,132 +1,129 @@
 import random
-import time
 
 import pytest
+from faker import Faker
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
-from conftest import chrome_browser as driver
-from conftest import get_webdriver_instance_and_open_my_account_page as preparation_work_account_page
-from conftest import get_webdriver_instance_and_open_catalog_and_sub_catalog_page as preparation_work_catalog_page
-from conftest import get_username_password
-from pages.product_card_page import get_ordering_product
-from pages.product_card_page import get_any_product_from_catalog
-from pages.product_card_page import ProductPage
-from pages.shopping_cart_page import CartPage
+from pages.my_account_page import MyAccountPage
 from pages.catalog_and_category_page import CatalogAndCategoryPage
+from pages.product_card_page import ProductPage
+from pages.product_card_page import get_any_product_from_catalog, get_ordering_product
+from pages.shopping_cart_page import CartPage
 
 
-@pytest.mark.parametrize("count_of_products", [0, 11])
-@pytest.mark.xfail(reason="If product haven't in a stock then we could get error")
-def test_change_count_buying_product(preparation_work_catalog_page, count_of_products):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, count_of_products)
+@pytest.mark.usefixtures('chrome_browser')
+class TestsProductCard:
 
-    if len(product_title) > 0:
-        product_page = ProductPage(driver, product_title)
+    @pytest.mark.parametrize("count_of_products", [0, 11])
+    def test_change_count_buying_product(self, catalog_and_sub_catalog_page: CatalogAndCategoryPage, count_of_products: int):
+        driver, product_title = get_any_product_from_catalog(catalog_and_sub_catalog_page, count_of_products)
 
-        if product_page.is_quantity_field_available() and product_page.is_available_in_stock():
-            product_page.change_count_buying_product(count_of_products)
+        if len(product_title) > 0:
+            product_page: ProductPage = ProductPage(driver, product_title)
+
+            if product_page.is_quantity_field_available() and product_page.is_available_in_stock():
+                product_page.change_count_buying_product(count_of_products)
+            else:
+                product_page.logger.error("Product out of stock or cannot define quantity of products in stock")
+
+    def test_add_product_to_cart(self, product_page: ProductPage):
+        product, product_title = product_page
+
+        if product.is_available_in_stock():
+            product.add_product_to_cart()
+            success_message: str = product.get_text_of_element(product.SUCCESS_MESSAGE_AFTER_ADD_TO_CART)
+            success_message: str = success_message.split("\n")[1]
+
+            assert product_title in success_message, product_page.logger.error("Couldn't add product to cart")
         else:
-            print("Product out of stock or cannot define quantity of products in stock")
+            product_page.logger.error("Product was finished in stock")
 
+    def test_zoom_product_with_magnifying_glass_on_product_card(self, product_page: ProductPage):
+        product, product_title = product_page
 
-def test_add_product_to_cart(preparation_work_catalog_page):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, 9)
-    product = ProductPage(driver, product_title)
+        if product.is_magnifying_glass_available():
+            product.click_to_magnifying_glass()
 
-    if product.is_available_in_stock():
-        product.add_product_to_cart()
-        success_message = product.get_text_of_element(product.SUCCESS_MESSAGE_AFTER_ADD_TO_CART)
-        success_message = success_message.split("\n")[1]
+            img_title: str = product.get_text_of_element((By.XPATH, "//div[@class='pswp__caption']//div[1]"))
+            assert ".jpg" in img_title or \
+                   ".jpeg" in img_title or \
+                   ".png" in img_title, product.logger.error("Couldn't open image")
 
-        assert product_title in success_message, "Couldn't add product to cart"
-    else:
-        print("Product was finished in stock")
+    @pytest.mark.parametrize("mark, comment", [
+        (1, "Не советую, мне не понравилось"),
+        (2, "Так себе, можно найти и по лучше за такую цену"),
+        (3, "В целом всё норм, ничего плохого не могу сказать, получил то что заказывал и пожалуй могу посоветовать"),
+        (4, "Не плохая вешь, мне понравилось, однозначно могу посоветовать, берите"),
+        (5, "Берите не пожалейте, меня устраивает, уже несколько лет пользуюсь пользуюсь")
+    ])
+    def test_leave_feedback_for_product(self, account_page_with_auth: MyAccountPage, mark: int, comment: str):
+        product_title: str = get_ordering_product(account_page_with_auth, '', '', random.randint(0, 9))
+        product: ProductPage = ProductPage(account_page_with_auth.driver, product_title)
+        product.switch_to_feedback_tab()
 
+        if product.is_comment_field_available():
+            if product.is_exist_feedback(comment):
+                while True:
+                    fake_ru = Faker('ru-RU')
+                    comment = fake_ru.sentence(random.randint(1, 100))
 
-def test_zoom_product_with_magnifying_glass_on_product_card(preparation_work_catalog_page):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, 10)
-    product = ProductPage(driver, product_title)
+                    if not product.is_exist_feedback(comment):
+                        break
 
-    if product.is_magnifying_glass_available():
-        product.click_to_magnifying_glass()
+            product.leave_feedback(mark, comment)
 
-        img_title = product.get_text_of_element((By.XPATH, "//div[@class='pswp__caption']//div[1]"))
-        assert ".jpg" in img_title or\
-               ".jpeg" in img_title or\
-               ".png" in img_title, "Couldn't open image"
+            if product.driver.current_url == product.BASE_URL + "wp-comments-post.php":
+                product.go_back_in_detect_duplicate_feedback()
+                # print("Couldn't add feedback cause of duplicate")
+                product.logger.error("Couldn't add feedback cause of duplicate'", exc_info=True)
 
+            assert product.is_exist_feedback(comment), product.logger.error(f"Comment is not equal - {comment}")
+        else:
+            product.logger.error("Feedback block is unavailable")
 
-@pytest.mark.parametrize("mark, comment", [(1, "Не советую, мне не понравилось..."),
-                                           (2, "Так себе, можно найти и по лучше за такую цену"),
-                                           (3, "В целом всё норм, ничего плохого не могу сказать, получил то что заказывал и пожалуй могу посоветовать"),
-                                           (4, "Не плохая вешь, мне понравилось, однозначно могу посоветовать, берите"),
-                                           (5, "Берите не пожалейте, меня устраивает, уже несколько лет пользуюсь пользуюсь")])
-@pytest.mark.xfail(reason="Only in duplicate case")
-def test_leave_feedback_for_product(preparation_work_account_page, mark, comment):
-    my_account_page = preparation_work_account_page
-    username, password = get_username_password()
-    product_title = get_ordering_product(my_account_page, username, password, 4)
+        product.logout_by_link()
 
-    product = ProductPage(my_account_page.driver, product_title)
-    product.switch_to_feedback_tab()
-    if product.is_comment_field_available():
-        product.leave_feedback(mark, comment)
+    # Блок "Категория товаров"
+    def test_go_to_catalog_or_sub_catalog_in_side_block_on_product_page(self, product_page: ProductPage):
+        product, product_title = product_page
 
-        if product.driver.current_url == "https://intershop5.skillbox.ru/wp-comments-post.php":
-            product.go_back_when_detect_duplicate_feedback()
-            print("Couldn't add feedback cause of duplicate")
-            assert False
+        categories: list[WebElement] = product.get_categories_from_goods_category_block()
+        category_link: WebElement = product.get_element_from_another_element(categories[0], By.TAG_NAME, "a")
+        title_category: str = category_link.text.capitalize()
+        product.click_by(category_link)
+        category_page: CatalogAndCategoryPage = CatalogAndCategoryPage(product.driver, title_category)
 
-        assert product.is_exist_feedback(comment), "Couldn't leave feedback"
-    else:
-        print("Feedback block is unavailable")
+        assert title_category == category_page.get_title(), category_page.logger.error("Couldn't go to category page")
 
+    # Блок "Сопутствующие товары"
+    def test_go_to_product_from_related_products(self, product_page: ProductPage):
+        product, product_title = product_page
+        wd, related_product_title = product.go_to_related_product()
+        related_product_in_new_page: ProductPage = ProductPage(wd, related_product_title)
 
-# Блок "Категория товаров"
-def test_go_to_catalog_sub_catalog_in_side_block_on_product_page(preparation_work_catalog_page):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, 10)
-    product = ProductPage(driver, product_title)
+        assert related_product_title.capitalize() in related_product_in_new_page.get_title(), \
+            related_product_in_new_page.logger.error("Couldn't go to related product")
 
-    categories = product.get_categories_from_goods_category_block()
-    category_link = product.get_element_from_another_element(categories[0], By.TAG_NAME, "a")
-    title_category = category_link.text.capitalize()
-    product.click_by(category_link)
-    category_page = CatalogAndCategoryPage(product.driver, title_category)
+    def test_add_related_product_to_cart(self, catalog_and_sub_catalog_page: CatalogAndCategoryPage):
+        driver, product_title = get_any_product_from_catalog(catalog_and_sub_catalog_page, 6)
+        product_page: ProductPage = ProductPage(driver, product_title)
 
-    assert title_category == category_page.get_title(), "Couldn't go to category page"
+        product_page.driver, related_product_title = product_page.add_related_product_to_cart()
+        product_page.go_to_cart_after_add_related_product()
+        shopping_cart: CartPage = CartPage(driver)
 
+        assert related_product_title in shopping_cart.get_product_title_by(related_product_title), \
+            shopping_cart.logger.error("Couldn't add related product in cart")
 
-# Блок "Сопутствующие товары"
-def test_go_to_product_from_related_products(preparation_work_catalog_page):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, 6)
-    product = ProductPage(driver, product_title)
-    wd, related_product_title = product.go_to_related_product()
-    related_product_in_new_page = ProductPage(wd, related_product_title)
+    # Блок "Товары"
+    @pytest.mark.parametrize("index", [0, 2])
+    def test_go_to_product_from_products_sidebar_on_product_page(self, product_page: ProductPage, index: int):
+        product_p, product_title = product_page
+        products_in_goods_block: list[WebElement] = product_p.get_all_products_from_goods_block()
+        product: WebElement = products_in_goods_block[index]
+        product_title: str = product.text
+        product.click()
+        product_card: ProductPage = ProductPage(product_p.driver, product_title)
 
-    assert related_product_title in related_product_in_new_page.get_title(), "Couldn't go to related product"
-
-
-def test_add_related_product_to_cart(preparation_work_catalog_page):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, 6)
-    product_page = ProductPage(driver, product_title)
-    product_page.driver, related_product_title = product_page.add_related_product_to_cart()
-    product_page.go_to_cart_after_add_related_product()
-    shopping_cart = CartPage(driver)
-
-    assert related_product_title in shopping_cart.get_product_title_by(related_product_title),\
-        "Couldn't add related product in cart"
-
-
-# Блок "Товары"
-@pytest.mark.parametrize("index", [0, 2])
-def test_go_to_product_from_products_sidebar_on_product_page(preparation_work_catalog_page, index):
-    driver, product_title = get_any_product_from_catalog(preparation_work_catalog_page, 6)
-    product_page = ProductPage(driver, product_title)
-
-    products_in_goods_block = product_page.get_all_products_from_goods_block()
-    product = products_in_goods_block[index]
-    product_title = product.text
-    product.click()
-    product_card = ProductPage(product_page.driver, product_title)
-
-    assert product_card.get_title() == product_title, "Couldn't find product and go to product page"
+        assert product_card.get_title() == product_title.capitalize(), product_card.logger.error("Couldn't find product and go to product page")
